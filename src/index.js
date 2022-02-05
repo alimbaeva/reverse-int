c655a38598f393459e4cea6419f1f38f5c04a2d4
Asel <asel.alimbaeva.kk@gmail.com>
2022-02-05 16:47:55 +0300

    feat: finish function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    // let str=n;
+    // return n < 0? Math.abs(n): 
+    let str = String(Math.abs(n));
+    if (str[str.length - 1] === '0') {
+        str = str.slice(0, str.length - 1);
+        return Number(str.split('').reverse().join(''));
+    } else { return Number(str.split('').reverse().join('')); }
 }
